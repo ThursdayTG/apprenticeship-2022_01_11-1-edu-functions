afa9cca4966a4f971124cdedc474d95d195b86df
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-02-06 21:47:31 +0100

    Update and rename main-source-b004.cpp to main-source-b005.cpp

--- main-source-b005.cpp
+++ main-source-b005.cpp
@@ -10,6 +10,8 @@
 void	subRoutine1();
 void	subRoutine2(int, int);
 int		subRoutine3(int, int, int);
+int		subRoutine3(int, int);
+// Überladen heißt, dass mehrere Funktionen mit dem gleichen Namen hinterlegt sind. Der Compiler ist aber schlau genug die Funktionen anhand der Übergabeparameter zu unterscheiden. Er kann sowohl Anzahl als auch Typ unterscheiden.
 
 /*
  * a "function prototype" or "function interface" declares a function, but omits the function body.
@@ -56,6 +58,10 @@ int main() {
 		cout	<< " sum sR3 sum = "
 				<< subRoutine3(12, 17, 3)
 				<< " \n";
+
+		cout	<< " sum sR3 dif = "
+				<< subRoutine3(12, 17)
+				<< " \n";
 		/*	primary execution block	*/
 
 		/*	end block	*/
@@ -94,21 +100,33 @@ int subRoutine3(int x, int y, int z) {
 	return sum;
 }
 
+// overloading functions
+int subRoutine3(int x, int y) {
+	int		difference = x - y;
+	return difference;
+}
+
+// recursive functions
+int subRoutine4(int x, int y) {
+	int		difference = x - y;
+	return difference;
+}
+
 
 
 /*	using C++ compiler from GCC via console
 
 	compile for debug:
-g++ -Og main-source-b004.cpp -o main-newest.debug
+g++ -Og main-source-b005.cpp -o main-newest.debug
 
 	clear and compile as final executable:
-clear && g++ -O3 main-source-b004.cpp -o main-newest.release
+clear && g++ -O3 main-source-b005.cpp -o main-newest.release
 
 	clear console, compile debug executable, compile release executable, and run program:
-g++ -Og main-source-b004.cpp -o main-newest.debug && g++ -O3 main-source-b004.cpp -o main-newest.release && clear && ./main-newest.release
+g++ -Og main-source-b005.cpp -o main-newest.debug && g++ -O3 main-source-b005.cpp -o main-newest.release && clear && ./main-newest.release
 
-   g++ -Og main-source-b004.cpp -o main-newest.debug
-&& g++ -O3 main-source-b004.cpp -o main-newest.release
+   g++ -Og main-source-b005.cpp -o main-newest.debug
+&& g++ -O3 main-source-b005.cpp -o main-newest.release
 && clear
 && ./main.release
 */
